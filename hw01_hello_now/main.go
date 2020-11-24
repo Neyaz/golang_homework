b6d29f48af9043a1fea7f9a237ca4615bff290f3
Nikolay Malinin <ne9zzz@gmail.com>
2020-11-24 23:01:39 +0300

    Remove concatanation

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -14,6 +14,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("current time: " + time.Now().Round(time.Nanosecond).String())
-	fmt.Println("exact time: " + exactTime.Round(time.Nanosecond).String())
+	fmt.Println("current time:", time.Now().Round(time.Nanosecond).String())
+	fmt.Println("exact time:", exactTime.Round(time.Nanosecond).String())
 }
